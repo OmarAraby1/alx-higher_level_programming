42929c38588d9f4cb1218751811556d7185b812b
Omar Araby <72764839+OmarAraby1@users.noreply.github.com>
2023-09-05 16:23:43 +0300

    Check Random Number

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number == 0:
+	print(number, "is zero")[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[number," is zero")
+elif number < 0:
+	print(number," is negative")
+else:
+	print(number," is positive")
