add401142402a6088dc73549e8ac05b7e5ae0055
Omar Araby <omararaby2001@gmail.com>
2023-09-07 22:30:08 +0300

    sys argc & argv like c programming

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print argc & argv like c programming."""
+    """Print the number of and list of arguments."""
     import sys
 
     argc = len(sys.argv) - 1
     if argc == 0:
-	print("0 arguments.")
+        print("0 arguments.")
     elif argc == 1:
-	print("1 argument:")
+        print("1 argument:")
     else:
-	print("{} arguments:".format(argc))
+        print("{} arguments:".format(argc))
     for i in range(argc):
-	print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
