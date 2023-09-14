cddd64c08c07a9c9b182d369e9a35b387c638bfe
Omar Araby <omararaby2001@gmail.com>
2023-09-14 21:55:34 +0300

    square values of matrix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map((lambda i: i ** 2), map((lambda rows: rows), matrix)))
+    return list(map(lambda rows: list(map(lambda i: i ** 2, rows)), matrix))
