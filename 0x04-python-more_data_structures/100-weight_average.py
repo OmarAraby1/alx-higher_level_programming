f2058592457b3a24a647bab7fe37b76ad44079ab
Omar Araby <omararaby2001@gmail.com>
2023-09-14 21:53:52 +0300

    weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if not len(my_list):
-        return 0 
+        return 0
     sum = 0
-    den = 0 
+    den = 0
     for tup in my_list:
         sum += tup[0] * tup[1]
         den += tup[1]
