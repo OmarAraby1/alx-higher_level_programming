2e83b872301b4d44e0b947aee5955cd8a5d7d436
Omar Araby <omararaby2001@gmail.com>
2023-09-14 18:59:02 +0300

    find max value in dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,11 @@
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
+
     k = list(a_dictionary.keys())[0]
     mx = a_dictionary[k]
     for key, val in a_dictionary.itmes():
         if val > mx:
             mx = val
             k = key
-    return k
+    return (k)
