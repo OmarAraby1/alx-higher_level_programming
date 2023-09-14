83976f9d6cb9c9ab41069bfd6eea35578a62b833
Omar Araby <omararaby2001@gmail.com>
2023-09-14 13:52:30 +0300

    convert to uppercase without upper()

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,6 @@
 def uppercase(str):
     for letter in str:
         if ord(letter) >= 97 and ord(letter) <= 122:
-            letter = ord(letter) - 32
-            print("{}".format(chr(letter)), end="")
-        else:
-            print("{}".format(letter), end="")
+            letter = chr(ord(letter) - 32)
+        print("{}".format(letter), end="")
+    print("")
