7efd55b66d66e70bc1bd0c123aa98eb6e0ef3aee
Omar Araby <omararaby2001@gmail.com>
2023-09-14 17:43:58 +0300

    check if even or odd

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     for i in range(len(my_list)):
-        if i % 2 == 0:
-            new_list[i] = True
+        if my_list[i] % 2 == 0:
+            new_list = True
         else:
-            new_list[i] = False
+            new_list = False
     return new_list
