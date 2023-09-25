211d746b1a177f36c10ab7c9bcea66315a48b95e
Omar <omararaby2001@gmail.com>
2023-09-25 03:32:07 -0700

    HANDLE IndexError

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 
 def safe_print_list(my_list=[], x=0):
     len = 0
-    for i in range(x + 1):
+    for i in range(x):
         try:
             print(f"{my_list[i]}", end="")
             len += 1
