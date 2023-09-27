3d6eaa8d9f820970bc32b1ca5ec3bb653e452eee
Omar <omararaby2001@gmail.com>
2023-09-27 15:09:58 -0700

    add another private tuple attribute

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,7 +5,7 @@
 
 class Square:
     """Represent a square."""
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.__position = position
 
