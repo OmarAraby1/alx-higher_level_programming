54c6cb03e4b2090379a096593d29cce7d1da178d
Omar <omararaby2001@gmail.com>
2023-09-27 15:13:13 -0700

    add another private tuple attribute

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -40,9 +40,10 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print("")
-        else:
-            [print("") for i in range(0, self.__position[1])]
-            for i in range(self.__size):
-                [print(" ", end="") for j in range(0, self.__position[0])]
-                [print("#", end="") for j in range(self.__size)]
-                print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for j in range(self.__size)]
+            print("")
