678b62a406f51d42707b8d14c2ff4641beb57750
Omar Araby <72764839+OmarAraby1@users.noreply.github.com>
2024-03-15 16:33:26 +0200

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/3-force_name.sql
+++
@@ -1,2 +0,0 @@
--- creates table force_name with id and name
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
--- 0x0E-SQL_more_queries/4-never_empty.sql
+++
@@ -1,2 +0,0 @@
--- creates table id_not_null on MySQL server with id and name
-CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
